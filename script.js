4c14bc982ed797eaa24bb64cf52c5d0a13fde6fb
Yevhen Tukubaiev <etukubaev2@gmail.com>
2022-11-18 01:42:47 +0200

    Task done.

--- script.js
+++ script.js
@@ -2,28 +2,42 @@ let rowNum = 0;
 let appendCall = 0;
 
 function paintGeneration() {
+	let color = '#A8889E';
+	if (rowNum % 2 == 0)
+		color = '#00CC00';
+	if (rowNum == 0)
+		color = '#CC00CC';
+	$("#random-table").map(function(index, elem) { return $(elem).children()[rowNum]}).css('background-color', color);
 	return;
 }
 
 function randomDataRow() {
-	return "<tr><td>" + (Math.random() * 42) + "</tr></td>";
+	return "<tr><td>" + Math.random() * 42 + "</tr></td>";
 }
 
 function runGeneration() {
-	//if (rowNum++ == 0) {
-		$("#random-table").append(randomDataRow());
-	//}
+	$("#random-table").append(randomDataRow());
+	paintGeneration();
+	rowNum += 1;
 	return;
 }
 
-// function stopGeneration()
 jQuery(document).ready(function() {
 	$("#run-generation").click(function() {
-		runGeneration();
-		appendCall = setInterval(runGeneration, 420);
+		// run only one instance
+		if (!appendCall) {
+			let appendInterval = parseFloat($("#append-interval").val());
+			appendInterval = (appendInterval) ? appendInterval * 1000 : 0.42 * 1000;
+			runGeneration();
+			appendCall = setInterval(
+				runGeneration,
+				appendInterval	
+			);
+		}
 	});
 	
 	$("#stop-generation").click(function() {
 		clearInterval(appendCall);
+		appendCall = 0;
 	});
 });
